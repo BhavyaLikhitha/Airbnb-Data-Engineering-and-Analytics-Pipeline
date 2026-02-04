d90c95bd350b12a51ea5f4339bf9debdab6908ca
BhavyaLikhitha <bhavyalikhitha.bbl@gmail.com>
2026-02-03 20:18:46 -0500

    bronze layer

---
+++ airbnb_dbt_pipeline/models/bronze/bronze_listings.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='incremental') }}
+{# 
+{% set incremental_flag = 1 %}
+{% set incremental_col = CREATED_AT %} #}
+
+SELECT * FROM  {{ source('staging', 'listings') }}
+
+{% if is_incremental() %}
+    WHERE CREATED_AT > (SELECT COALESCE(MAX(CREATED_AT), '1900-01-01') FROM {{ this }})
+{% endif %}
